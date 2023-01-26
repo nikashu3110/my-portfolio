5436386d5b4b8d3322b34581c9e7b8b6056f6ed9
Ashutosh <ashutoshnikalje31@gmail.com>
2023-01-26 23:15:03 +0530

    About section done

--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -1,21 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
+import Greet from "../content/Greet";
 import "../main/Main.css";
 
 export default function Main() {
-  return (
-    <>
-      <div className="container">
-        <div className="nav">
-          <ul>
-            <a href="/">ABOUT</a>
-            <a href="/">PROJECTS</a>
-            <a href="/">CONTACT</a>
-            <a href="/">RESUME</a>
-          </ul>
-        </div>
+  const greetings = [
+    { message: "Hello", language: "English" },
+    { message: "Bonjour", language: "French" },
+    { message: "Hola", language: "Spanish" },
+    { message: "Ciao", language: "Italian" },
+  ];
+  const [greeting, setGreeting] = useState(greetings[0]);
+  const randomizer = () => {
+    const randomIndex = Math.floor(Math.random() * greetings.length);
+    setGreeting(greetings[randomIndex]);
+  };
 
-        
-      </div>
-    </>
+  return (
+    <div className="container">
+      <section className="about" id="about">
+        <div className="about-desc">
+          <h1 onClick={randomizer}>
+            {greeting.message} <span className="wave">👋</span>
+          </h1>
+          <h2>
+            My name is <span className="name">Ashutosh Nikalje</span>
+          </h2>
+          <p>
+            I am a Front-End Developer. My projects are mainly focused on{" "}
+            Web-Development. I have a experience working in <span>ReactJs</span>
+            , <span>HTML</span>, <span>CSS</span>, <span>JavaScript</span> and{" "}
+            <span>Java</span>. I also have decent knowledge of{" "}
+            <span>Angular</span>, <span>PHP</span>, <span>MySQL</span> and{" "}
+            <span>Prototyping</span>. My goal is become a full stack developer
+          </p>
+        </div>
+      </section>
+    </div>
   );
 }
